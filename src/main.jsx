f904b0f358abd15c69f968b8cdd0ac14230e2959
Adnan-hamid-wani <Adnanaduuu@gmail.com>
2024-11-26 12:06:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+        <AuthProvider>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
+              </AuthProvider>
+
   </StrictMode>
 );
